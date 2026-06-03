49f420892677f74cc824465320ee766594f15297
Alessandro Ardenghi <154625536+alessandroardenghi@users.noreply.github.com>
2026-06-03 23:14:15 +0200

    [CHORE]: Cleaned Functions and Main

--- src/utils/neighborhood_cleaning.py
+++ src/utils/neighborhood_cleaning.py
@@ -1,20 +1,33 @@
 import pandas as pd
 
 
-def normalize_neighborhood(df: pd.DataFrame, threshold: int = 100) -> pd.DataFrame:
-    milan_counts = df[df["city"] == "MI"]["neighbourhood_cleansed"].value_counts()
-    major_milan_neighborhoods = milan_counts[milan_counts >= threshold].index
+def normalize_neigh_col(df: pd.DataFrame, threshold: int = 100) -> pd.DataFrame:
 
+    # Computing the number of occurrencies of each neigh. in MI
+    milan_neigh_counts = df[df["city"] == "MI"]["neighbourhood_cleansed"].value_counts()
+
+    # Filtering only the neighborhoods with at least "threshold" occurrences
+    major_milan_neighborhoods = milan_neigh_counts[
+        milan_neigh_counts >= threshold
+    ].index
+
+    # Defining custom function to assign each neighborhood to a bucket
     def categorize_location(row):
+
+        # For Bergamo, we bucket all towns into either Bergamo City or Province
         if row["city"] == "BG":
+
             if row["neighbourhood_cleansed"] == "Bergamo":
                 return "Bergamo_City"
+
             else:
                 return "Bergamo_Province"
 
         elif row["city"] == "MI":
+
             if row["neighbourhood_cleansed"] in major_milan_neighborhoods:
                 return row["neighbourhood_cleansed"]
+
             else:
                 return "Milan_Other"
 
